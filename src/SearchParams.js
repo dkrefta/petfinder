334c25a44c6079299a586b1a98977b6bf612e112
Diego Krefta <diego.krefta@gmail.com>
2018-08-28 13:14:28 -0300

    SearchParams adjusts

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,53 @@
 import React, { Component } from "react";
+import pf, { ANIMALS } from "petfinder-client";
+
+const petfinder = pf ({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+})
 
 export default class SearchParams extends Component {
   state = {
     location: "Seattle, WA",
     animal: "",
-    breed: ""
+    breed: "",
+    breeds: []
   };
+
+  handleLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  };
+  handleAnimalChange = event => {
+    this.setState({
+      animal: event.target.value
+    });
+  };
+
+  getBreeds() {
+    if (this.state.animal) {
+      petfinder.breed.list({ animal: this.state.animal }).then(data => {
+        if (
+          data.petfinder &&
+          data.petfinder.breeds &&
+          Array.isArray(data.petfinder.breeds.breed)
+        ) {
+          this.setState({
+            breeds: data.petfinder.breeds.breed
+          });
+        } else {
+          this.setState({
+            breeds: []
+          });
+        }
+      });
+    } else {
+      this.setState({
+        breeds: []
+      });
+    }
+  }
 
   render() {
     return (
@@ -13,10 +55,28 @@ export default class SearchParams extends Component {
         <label htmlFor="location">
           Location
           <input
+            onChange={this.handleLocationChange}
             id="location"
             value={this.state.location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={this.state.animal}
+            onChange={this.handleAnimalChange}
+            onBlur={this.handleAnimalChange}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {" "}
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
       </div>
     );
