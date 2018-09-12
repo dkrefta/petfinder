1fe1a1cb49680eec98904863be1647b969e8c3e0
Diego Krefta <diego.krefta@gmail.com>
2018-09-11 22:03:09 -0300

    Code Refactor

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
+import Results from "./Results";
+import Details from "./Details";
 import { Router } from "@reach/router";
 import pf from "petfinder-client";
-import Loadable from 'react-loadable';
 import { Provider } from "./SearchContext";
+import SearchParams from "./SearchParams";
 import NavBar from "./NavBar";
-
-const LoadableDetails = Loadable({
-  loader: () => import('./Details'),
-  loading() {
-    return <h1> loading split out code ...</h1>;
-  }
-})
-
-const LoadableResults = Loadable({
-  loader: () => import('./Results'),
-  loading() {
-    return <h1> loading split out code ...</h1>;
-  }
-})
-
-const LoadableSearchParams = Loadable({
-  loader: () => import('./SearchParams'),
-  loading() {
-    return <h1> loading split out code ...</h1>;
-  }
-})
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -97,9 +78,9 @@ class App extends React.Component {
         <NavBar />
         <Provider value={this.state}>
           <Router>
-            <LoadableResults path="/" />
-            <LoadableDetails path="/details/:id" />
-            <LoadableSearchParams path="/search-params" />
+            <Results path="/" />
+            <Details path="/details/:id" />
+            <SearchParams path="/search-params" />
           </Router>
         </Provider>
       </div>
